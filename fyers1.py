405c71a552c6cdde0926e66eb4c70b350078f3c8
nirajvijay <67600212+nirajvijay@users.noreply.github.com>
2023-06-27 16:55:33 +0530

    Update fyers1.py

--- fyers1.py
+++ fyers1.py
@@ -15,11 +15,11 @@ pin2 = '7'
 pin3 = '2'
 pin4 = '7'
 
-session = accessToken.SessionModel(client_id, secret_key, redirect_uri)
+session = accessToken.SessionModel(client_id, secret_key)
 
 
 def generate_auth_code():
-    session = accessToken.SessionModel(client_id, secret_key, redirect_uri)
+    session = accessToken.SessionModel(client_id, secret_key)
     response = session.generate_authcode()
     driver = webdriver.Chrome()
     driver.get(response)
